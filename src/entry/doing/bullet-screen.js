8ec8ad203dfc1d928ed46bf9a206c3ed242c4afb
ljjkerwin <ljjkerwin@gmail.com>
2018-07-30 00:20:21 +0800

    pure js BulletScreen

--- src/entry/doing/bullet-screen.js
+++ src/entry/doing/bullet-screen.js
@@ -13,16 +13,16 @@ let headImgUrl = 'https://img.qlchat.com/qlLive/userHeadImg/A346CA7B-7F38-4069-A
 
 export default class BulletScreen extends React.Component {
 	static defaultProps = {
-        getBulletsInterval: 30,
-        roleList: [],
+		getBulletsInterval: 30,
+		roleList: [],
 	}
 
 	conf = {
-        // 显示区域宽度
-        wrapWidth: 0,   
-        // 每条弹幕的行高
-        lineHeight: 0,
-        // 每条弹幕的滚动时间
+		// 显示区域宽度
+		wrapWidth: 0,
+		// 每条弹幕的行高
+		lineHeight: 0,
+		// 每条弹幕的滚动时间
 		oneRoundTime: 10000,
 	}
 
@@ -43,14 +43,14 @@ export default class BulletScreen extends React.Component {
 		// 发送弹幕请求状态
 		addBulletStatus: '',
 
-        // 弹幕播放的状态
+		// 弹幕播放的状态
 		isPlay: false,
 
 		// 所有弹幕对象池
 		bulletMap: {},
 		// 待显示弹幕id队列
 		bulletWaitIds: [],
-        // 每行的状态
+		// 每行的状态
 		rowsStatus: [
 			/**
 			{ 
@@ -69,58 +69,39 @@ export default class BulletScreen extends React.Component {
 		bulletInputText: '',
 	}
 
-	componentWillMount() {window.bs = this
-		// 读取开关状态
-		let isSwitchOn = true;
-		try {
-			if (localStorage.getItem('BULLET_SCREEN_SWITCH_OFF') > 0) {
-				isSwitchOn = false;
-			}
-		} catch (e) {}
-		this.setState({
-			isSwitchOn,
-		})
-    }
-    
-    componentDidMount() {
-        this.play()
-    }
+	componentDidMount() {
+		this.play()
+	}
 
 	componentWillReceiveProps(nextProps) {
 		if (!this.state.isSwitchOn) return;
-        if (this.props.currentTime !== nextProps.currentTime) {
-            this.handleUpdateCurrentTime(Math.floor(nextProps.currentTime));
-        }
-    }
-    
-    componentWillUpdate() {
-        // this.__=Date.now()
-    }
-    componentDidUpdate() {
-        // log(Date.now() - this.__)
-    }
+		if (this.props.currentTime !== nextProps.currentTime) {
+			this.handleUpdateCurrentTime(Math.floor(nextProps.currentTime));
+		}
+	}
+
 
 	render() {
 		let { bulletMap, rowsStatus, isPlay } = this.state;
 		let now = Date.now();
 
 		return <Fragment>
-            {
-                this.state.isSwitchOn &&
-                <TransitionGroup>
-                    {
-                        this.state.isShowMask &&
-                        <CSSTransition
-                            classNames="co-bullet-screen"
-                            timeout={{enter: 1000, exit: 300}}
+			{
+				this.state.isSwitchOn &&
+				<TransitionGroup>
+					{
+						this.state.isShowMask &&
+						<CSSTransition
+							classNames="co-bullet-screen"
+							timeout={{ enter: 1000, exit: 300 }}
 							onEntered={this.onEntered}
 							onExit={this.onExited}
-                        >
+						>
 							<div className="co-bullet-screen"
 								ref={r => this.refWrap = r}>
-                                <div className="reference" ref={r => this.refRefer = r}></div>
-                                <div className="bullet-screen-layer">
-                                    {rowsStatus.map(row => {
+								<div className="reference" ref={r => this.refRefer = r}></div>
+								<div className="bullet-screen-layer">
+									{rowsStatus.map(row => {
 										return row.bullets.map((bulletStatus, index) => {
 											let preBulletStatus = index ? row.bullets[index - 1] : null;
 											return <Bullet
@@ -135,137 +116,18 @@ export default class BulletScreen extends React.Component {
 											/>;
 										})
 									})}
-                                </div>
-                            </div>
-                        </CSSTransition>
-                    }
-                </TransitionGroup>
-            }
-
-			{
-				this.props.bulletScreenControllerWrap &&
-					createPortal(
-						<div className="co-bullet-screen-controller">
-							<div className={`btn-bullet on-log${this.state.isSwitchOn ? ' active' : ''}`}
-								 onClick={this.state.isSwitchOn ? this.onClickSwitchOff : this.onClickSwitchOn}
-								 data-log-region={this.state.isSwitchOn ? "bullet-screen-btn-off" : "bullet-screen-btn-on"}
-							><span></span></div>
-							{
-								this.state.isSwitchOn &&
-								<div className="btn-edit on-log" 
-									onClick={this.showSendBar}
-									data-log-region="bullet-screen-btn-edit"
-								><span></span></div>
-							}
-						</div>,
-						this.props.bulletScreenControllerWrap
-					)
+								</div>
+							</div>
+						</CSSTransition>
+					}
+				</TransitionGroup>
 			}
-			{
-				(this.state.isShowSendBar || this.state.bulletInputText) && this.props.mainContainer &&
-				createPortal(
-					<div className="co-bullet-screen-send-bar" ref={r => this.inputBoxWrap = r}>
-						<input type="text"
-						       value={this.state.bulletInputText}
-						       onChange={this.bulletInputChangeHandle}
-						       onBlur={this.bulletInputBlurHandle}
-						       placeholder={this.props.topicInfo.isBanned === 'Y' ? "本话题已被禁言" : "看来你想发个弹幕~"}
-							   autoFocus={true}
-							   disabled={this.props.topicInfo.isBanned === 'Y'}
-						/>
-						<div className="send-btn on-log"
-							onClick={this.sendBullet}
-							data-log-region="bullet-screen-btn-send"
-						>发送</div>
-					</div>,
-					this.props.mainContainer
-				)
-			}
-        </Fragment>
+
+		</Fragment>
 	}
 
-	showSendBar = () => {
-		this.setState({isShowSendBar: !this.state.isShowSendBar}, () => {
-			setTimeout(() => {
-				this.inputBoxWrap.scrollIntoView(true);
-			}, 200)
-		})
-	}
 
-	bulletInputChangeHandle(e){
-		this.setState({
-			bulletInputText: e.target.value
-		})
-	}
-
-	bulletInputBlurHandle(){
-		this.hideSendBarTimer = setTimeout(() => {
-			this.setState({
-				isShowSendBar: false
-			});
-		}, 300)
-	}
-
-    sendBullet(){
-		// 音频互动已结束情况下，只在本地发送
-		if (this.props.topicInfo.style === 'audio' && this.props.topicInfo.status === 'ended') {
-			let content = this.state.bulletInputText + '';
-			if (!content.length) return;
-			this.addBullet({
-				id: Math.random(),
-				content,
-				isCurrentSend: true,
-				headImgUrl: this.props.userInfo.headImgUrl,
-			}, true)
-
-			this.setState({
-				bulletInputText: ''
-			})
-			return;
-		}
-
-		if (this.state.addBulletStatus === 'pending') return;
-		let content = this.state.bulletInputText + '';
-		if (!content.length) return;
-
-		this.setState({addBulletStatus: 'pending'});
-		request({
-			url: '/api/wechat/bullet/add',
-			method: 'POST',
-			body: {
-				liveId: this.props.topicInfo.liveId,
-				topicId: this.props.topicInfo.id,
-				style: this.props.topicInfo.style,
-				playTime: Math.floor(this.props.currentTime || 0),
-				content,
-			}
-		}).then(res => {
-			if (res.state.code) throw Error(res.state.msg);
-
-			let bullet = res.data.bullet;
-
-			// 发送成功后高亮显示出来
-			this.addBullet({
-				...bullet,
-				playTime: 0,
-				isCurrentSend: true,
-			}, true)
-
-			this.setState({
-				bulletInputText: ''
-			});
-
-			window.toast('发送成功');
-		}).catch(err => {
-			console.error(err);
-			window.toast(err.message);
-		}).then(() => {
-			this.setState({addBulletStatus: ''});
-		})
-
-    }
-
-    handleUpdateCurrentTime(nextCurrentTime) {
+	handleUpdateCurrentTime(nextCurrentTime) {
 		let { preGetBulletsStartTime, preGetBulletsEndTime = 0 } = this.state;
 		/**
 		 * 后移大于5秒，视作向后拖动
@@ -279,13 +141,13 @@ export default class BulletScreen extends React.Component {
 		} else if (nextCurrentTime > this.props.currentTime + 5) {
 			// console.log(`${nextCurrentTime}s`, '向后拖动，部分清空队列');
 			this.cleanWaitQueue(nextCurrentTime);
-			this.setState({preGetBulletsStartTime: nextCurrentTime});
+			this.setState({ preGetBulletsStartTime: nextCurrentTime });
 		} else if (this.props.currentTime > nextCurrentTime + 5) {
 			// console.log(`${nextCurrentTime}s`, '向前拖动，清空队列，尝试请求新弹幕');
 			this.cleanWaitQueue();
 			this.getBullets(nextCurrentTime, true);
 		}
-    }
+	}
 
 	/**
 	 * 插入弹幕到等待队列
@@ -293,9 +155,9 @@ export default class BulletScreen extends React.Component {
 	 * @param {boolean} isUnshift 是否从头部插入
 	 */
 	addBullet = (bullet, isUnshift) => {
-        if (!bullet || bullet.length === 0 || !this.state.isSwitchOn) return;
+		if (!bullet || bullet.length === 0 || !this.state.isSwitchOn) return;
 
-		let bulletMap = {...this.state.bulletMap};
+		let bulletMap = { ...this.state.bulletMap };
 		let bulletWaitIds = [...this.state.bulletWaitIds];
 
 		if (isUnshift) {
@@ -322,88 +184,25 @@ export default class BulletScreen extends React.Component {
 	/**
 	 * 暂停弹幕播放接口
 	 */
-    pause = () => {
-        this.setState({isPlay: false})
-    }
+	pause = () => {
+		this.setState({ isPlay: false })
+	}
 
 	/**
 	 * 恢复播放状态接口
 	 */
-    play = () => {
-        this.state.isPlay || this.setState({isPlay: true}, () => {
-            this.handleInsertWork()
-            this.handleCleanWork()
-        })
+	play = () => {
+		this.state.isPlay || this.setState({ isPlay: true }, () => {
+			this.handleInsertWork()
+			this.handleCleanWork()
+		})
 	}
 
 	/**
-	 * 请求弹幕数据
-	 * @param {boolean} isForward 是否向前拖动的请求
+	 * 清空等待队列
 	 */
-	getBullets = (startTime = 0, isForward) => {
-		if (!this.state.isSwitchOn) return;
-		if (this.state.getBulletsStatus === 'pending') return;
-
-		let getBulletsInterval = this.props.getBulletsInterval;
-		// 为防止频繁向前拖动引起的大量数据请求，此值为true时，请求时间段会变小
-		if (isForward) {
-			getBulletsInterval = 20;
-		}
-
-		let endTime = startTime + getBulletsInterval;
-		if (startTime < 2) startTime = 0; // 向左拖到尽头时极端情况
-
-		this.setState({getBulletsStatus: 'pending'});
-        
-        Promise.resolve().then(res => {
-
-
-
-            let list = [
-                {id: Math.random(), headImgUrl, content: '阿斯顿发斯蒂芬撒地方'},
-            ]
-			this.addBullet(list);
-			
-
-		}).catch(err => {
-			console.error(err);
-		}).then(() => {
-			this.setState({
-				getBulletsStatus: '',
-				preGetBulletsStartTime: startTime,
-				preGetBulletsEndTime: endTime,
-			});
-		})
-	}
-
-    onClickSwitchOn = () => {
-        this.setState({
-            isSwitchOn: true
-        })
-		try { localStorage.setItem('BULLET_SCREEN_SWITCH_OFF', '') } catch(e) {}
-    }
-
-    onClickSwitchOff = () => {
-        this.setState({
-            isSwitchOn: false,
-			isShowMask: false,
-			hasShowMask: false,
-			bulletMap: {},
-			bulletWaitIds: [],
-			rowsStatus: [],
-
-			getBulletsStatus: '',
-			preGetBulletsStartTime: undefined,
-			preGetBulletsEndTime: undefined,
-		})
-		try { localStorage.setItem('BULLET_SCREEN_SWITCH_OFF', '1') } catch(e) {}
-    }
-
-    /**
-     * 清空等待队列
-     */
-    cleanWaitQueue = currentTime => {
-		let bulletMap = {...this.state.bulletMap};
+	cleanWaitQueue = currentTime => {
+		let bulletMap = { ...this.state.bulletMap };
 		let bulletWaitIds = [...this.state.bulletWaitIds];
 
 		let len = bulletWaitIds.length;
@@ -419,10 +218,10 @@ export default class BulletScreen extends React.Component {
 			bulletMap,
 			bulletWaitIds,
 		})
-    }
+	}
 
 	handleInsertWork = throttleDebounce(() => {
-        // console.log('insert work')
+		// console.log('insert work')
 		if (!this.state.isPlay) return;
 		if (!this.state.bulletWaitIds.length) return;
 
@@ -432,7 +231,7 @@ export default class BulletScreen extends React.Component {
 		// 弹幕背景还没显示 并 将有弹幕需要显示，提前显示弹幕背景，此处2是弹幕显示动画的时间
 		if (!this.state.isShowMask && !(bulletMap[bulletWaitIds[0]].playTime - 1 > currentTime)) {
 			this.setState({
-                isShowMask: true
+				isShowMask: true
 			});
 		}
 		// 没跑完弹幕背景显示动画，先不插入
@@ -505,11 +304,11 @@ export default class BulletScreen extends React.Component {
 
 	// 清除展示完的弹幕
 	cleanWorker = () => {
-        // console.log('clean work:', this.state.bulletMap, this.state.bulletWaitIds, this.state.rowsStatus)
-        this.cleanTimer = null;
-        if (!this.state.isPlay) return;
+		// console.log('clean work:', this.state.bulletMap, this.state.bulletWaitIds, this.state.rowsStatus)
+		this.cleanTimer = null;
+		if (!this.state.isPlay) return;
 
-		let bulletMap = {...this.state.bulletMap},
+		let bulletMap = { ...this.state.bulletMap },
 			rowsStatus = [...this.state.rowsStatus],
 			now = Date.now(),
 			oneRoundTime = this.conf.oneRoundTime + 300, // 加*取相对安全的单程时间
@@ -544,23 +343,23 @@ export default class BulletScreen extends React.Component {
 		if (hasRunningBullet) {
 			this.handleCleanWork();
 		}
-    }
+	}
 
-    onEntered = () => {
+	onEntered = () => {
 		this.state.isShowMask && this.setState({
 			hasShowMask: true,
 		}, () => {
 			this.initSize(this.play);
 		})
 	}
-	
+
 	onExited = () => {
 		this.state.isShowMask || this.setState({
 			hasShowMask: false,
 		})
 	}
 
-    initSize = cb => {
+	initSize = cb => {
 		let wrapWidth = this.refWrap.clientWidth;
 		let wrapHeight = this.refWrap.clientHeight;
 		let wrapViewLeft = this.refWrap.getBoundingClientRect().left;
@@ -574,9 +373,9 @@ export default class BulletScreen extends React.Component {
 			wrapWidth,
 			wrapViewLeft,
 			lineHeight,
-        }
+		}
 
-        // 初始化可用行状态
+		// 初始化可用行状态
 		let rowsStatus = [];
 		for (let i = 0; i < rowNum; i++) {
 			rowsStatus[i] = {
@@ -625,7 +424,7 @@ class Bullet extends React.Component {
 	render() {
 		let { data, status, conf } = this.props;
 		let { finalLeft, curLeft, curRoundTime } = this.state;
-		
+
 		let style = {
 			top: status.top,
 			transition: `transform ${curRoundTime === null ? conf.oneRoundTime : curRoundTime}ms linear`,
@@ -643,7 +442,7 @@ class Bullet extends React.Component {
 				style={style}
 				ref={r => this.dom = r}
 			>
-				<div className="avatar" style={{backgroundImage: `url(${imgUrlFormat(data.userHeadImg || data.headImgUrl || data.createByHeadImgUrl, '@60w_60h_1e_1c_2o')})`}}></div>
+				<div className="avatar" style={{ backgroundImage: `url(${imgUrlFormat(data.userHeadImg || data.headImgUrl || data.createByHeadImgUrl, '@60w_60h_1e_1c_2o')})` }}></div>
 				<div className="content">{this.state.content}</div>
 			</div>
 		);
@@ -680,7 +479,7 @@ class Bullet extends React.Component {
 	initData = () => {
 		// 截取content
 		let content = String(this.props.data.content);
-		content.length > 30 && (content = content.slice(0,30) + '...');
+		content.length > 30 && (content = content.slice(0, 30) + '...');
 
 		// 判断身份
 		let hasRole = this.props.roleList.indexOf(`${this.props.data.userId || this.props.data.createBy}`) >= 0;
@@ -700,19 +499,19 @@ class Bullet extends React.Component {
 		this.props.status.runNextBulletInterval = (domWidth + 10) / totalDistance * conf.oneRoundTime;
 		// 上一条结束前不会追上的间隔
 		this.props.status.catchUpPreBulletInterval = conf.oneRoundTime - conf.wrapWidth * conf.oneRoundTime / totalDistance;
-this.props.status.finalLeft =  -totalDistance
+		this.props.status.finalLeft = -totalDistance
 		this.setState({
 			finalLeft: -totalDistance,
 		}, () => {
-            
-		    this.judgeIfRun();
-        })
+
+			this.judgeIfRun();
+		})
 	}
 
 	judgeIfRun = () => {
 		if (!this.isRun && this.props.isPlay) {
 			// 前面没有弹幕，或距离前面弹幕为安全距离，开始运行生命周期
-		 	if (!this.props.preBulletStatus || Date.now() - this.props.preBulletStatus.startTime >= Math.max(this.props.preBulletStatus.runNextBulletInterval, this.props.status.catchUpPreBulletInterval)) {
+			if (!this.props.preBulletStatus || Date.now() - this.props.preBulletStatus.startTime >= Math.max(this.props.preBulletStatus.runNextBulletInterval, this.props.status.catchUpPreBulletInterval)) {
 				this.isRun = true;
 				this.play();
 			}
@@ -738,20 +537,20 @@ this.props.status.finalLeft =  -totalDistance
 			curRoundTime: this.props.conf.oneRoundTime - timeUsed,
 			curLeft: null
 		})
-			// 修正开始时间
-			this.props.status.startTime = Date.now() - timeUsed;
-            this.props.status.timeUsed = undefined;
-            
-            
-            // animation.add({
-            //     startValue: 0,
-            //     endValue:this.props.status.finalLeft,
-            //     duration: 5000,
-            //     step: (v) => {
-            //         this.dom.style.transform = `translate3d(${v}px,0,0)`;
-            //         // this.dom.style.left = `${v}px`;
-            //     }
-            // });
+		// 修正开始时间
+		this.props.status.startTime = Date.now() - timeUsed;
+		this.props.status.timeUsed = undefined;
+
+
+		// animation.add({
+		//     startValue: 0,
+		//     endValue:this.props.status.finalLeft,
+		//     duration: 5000,
+		//     step: (v) => {
+		//         this.dom.style.transform = `translate3d(${v}px,0,0)`;
+		//         // this.dom.style.left = `${v}px`;
+		//     }
+		// });
 	}
 }
 
@@ -761,16 +560,16 @@ this.props.status.finalLeft =  -totalDistance
 
 export const imgUrlFormat = (url, formatStrQ = "?x-oss-process=image/resize,m_fill,limit_0,h_64,w_64", formatStrW = "/64") => {
 
-    if (/(img\.qlchat\.com)/.test(url)) {
-        url = url.replace(/@.*/, "") + formatStrQ;
-    } else if (/(wx\.qlogo\.cn\/mmopen)/.test(url)) {
-        url = url.replace(/(\/(0|132|64|96)$)/, formatStrW);
-    };
+	if (/(img\.qlchat\.com)/.test(url)) {
+		url = url.replace(/@.*/, "") + formatStrQ;
+	} else if (/(wx\.qlogo\.cn\/mmopen)/.test(url)) {
+		url = url.replace(/(\/(0|132|64|96)$)/, formatStrW);
+	};
 
-    return url;
+	return url;
 };
 
 
 function log(c) {
-    oo.innerHTML =c + ','+oo.innerHTML;
+	oo.innerHTML = c + ',' + oo.innerHTML;
 }
--- src/entry/doing/index.js
+++ src/entry/doing/index.js
@@ -4,9 +4,91 @@ import React from 'react';
 import { render } from 'react-dom';
 import classNames from 'classnames';
 
-import throttleDebounce from 'modules/utils';
+import throttleDebounce from 'modules/utils/throttleDebounce';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
-import BulletScreen from './bullet-screen';
+// import BulletScreen from './bullet-screen';
+
+
+
+
+
+
+
+
+
+
+
+class BulletScreen {
+	constructor(conf = {
+		wrapDom,
+		rowHeight: 20,
+		oneRoundTime: 5000,
+		render: text => text,
+	}) {
+		if (!conf.wrapDom) throw Error('wrapDom 缺失');
+		conf.wrapWidth = conf.wrapDom.clientWidth;
+		this.conf = conf;
+
+		this.newLayer();
+	}
+
+	bulletQueue = []
+	rows = []
+
+	addBullet(bullet, isUnshift) {
+		if (!bullet || bullet.length === 0) return;
+
+		if (Array.isArray(bullet)) {
+			this.bulletQueue = this.bulletQueue.concat(bullet);
+		} else {
+			isUnshift ? this.bulletQueue.unshift(bullet) : this.bulletQueue.push(bullet);
+		}
+
+		this.handleInsertWork();
+	}
+
+	handleInsertWork = throttleDebounce(() => {
+		if (!this.bulletQueue.length) return;
+
+		const bullet = this.bulletQueue.shift();
+		const Bullet = this.newBullet(bullet);
+
+		setTimeout(() => {
+
+			Bullet.play();
+		})
+	}, 100)
+
+	newBullet(data) {
+		const dom = document.createElement('div');
+		dom.style = 'position: absolute; transform: translate3d(0,0,0);';
+		dom.innerHTML = data.content;
+		this.Layer.appendChild(dom);
+		const Bullet = {
+			dom,
+			play() {
+				this.dom.style.transition = `transform ${5000}ms linear`;
+				this.dom.style.transform = `translate3d(${400}px,0,0)`;
+			}
+		}
+		return Bullet;
+	}
+
+	newLayer() {
+		const dom = document.createElement('div');
+		dom.style = 'position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: 2';
+		this.conf.wrapDom.appendChild(dom);
+		this.Layer = dom;
+	}
+
+	unmount() {
+		this.Layer.remove();
+		this.Layer = null;
+	}
+}
+
+
+
 
 
 
@@ -19,31 +101,13 @@ class App extends React.Component {
 
 	componentDidMount() {
 		
-	
+		const bs = new BulletScreen({
+			wrapDom: document.getElementById('wrap')
+		});
 
-function addBullet(id) {
-	let b = document.createElement('div')
-	b.className='co-bullet'
-
-	b.innerHTML=`
-	<div class="avatar"></div>
-	<div class="content">35r23423</div>
-	`
-
-	document.getElementById(id).appendChild(b)
-
-	setTimeout(() => {
-		b.style.transition= `transform 5000ms linear`;
-		b.style.transform= `translate3d(${-1150}px,0,0)`;
-	},100)
-
-}
-
-setInterval(() => {
-	addBullet('l1')
-	// addBullet('l2')
-},2000)
-
+		bs.addBullet({
+			content: 234432234
+		});
 
 
 	}
@@ -54,16 +118,8 @@ setInterval(() => {
 
 			<div id="wrap">
 			
-				<div id='l1' style={{position: 'absolute',width: '100%', top: 0}}></div>
-				<div id='l2' style={{position: 'absolute',width: '100%', top: 100}}></div>
-			
 			</div>
 
-
-
-			<div id="oo" style={{wordBreak: 'break-all'}}>
-			</div>
-			
 		</div>
 	}
 }
@@ -73,5 +129,6 @@ setInterval(() => {
 render(
 <App/>
 , document.getElementById('root'))
+
 
 
